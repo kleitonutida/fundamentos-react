1273efa5aa0516a6e47e0548dcc05c2b071d6ad4
Kleiton Utida <kleiton.utida@live.com>
2023-02-27 00:14:38 -0300

    Components with params

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
-ReactDOM.render(<div><Primeiro></Primeiro></div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Primeiro></Primeiro>
+    <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={9.3} />
+  </div>,
+  document.getElementById("root")
+);
